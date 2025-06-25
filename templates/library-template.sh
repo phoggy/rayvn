25b1b011342e920659f73291dcb7b59a590499c8
phynix <phynix@phoggy.me>
2025-06-25 11:39:44 -0700

    Added init function to library template.

--- templates/library-template.sh
+++ templates/library-template.sh
@@ -12,6 +12,10 @@ my${libraryName}LibraryFunction () {
 
 PRIVATE_CODE="--+-+-----+-++(-++(---++++(---+( ⚠️ BEGIN PRIVATE ⚠️ )+---)++++---)++-)++-+------+-+--"
 
+_init_${projectName}_${name}() {
+    echo "library init_${projectName}_${name}() called"
+}
+
 # TODO remove/replace me!
 _my${libraryName}PrivateFunction() {
     echo "$(ansi bold _my${libraryName}PrivateFunction here!)"
