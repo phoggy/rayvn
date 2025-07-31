48d813417f9e66da9019f8117e9f7872a741818c
phynix <phynix@phoggy.me>
2025-07-31 10:21:47 -0700

    Add note strongly discouraging top-level code.

--- templates/library-template.sh
+++ templates/library-template.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-# Library of common functions.
+# My library.
 # Intended for use via: require ${qualifiedName}
 
+# IMPORTANT: Any output when sourcing libraries is treated as an error. This is required since
+#            parse/definition time errors (e.g. syntax) rarely result in a non-zero exit code,
+#            even with -e set. Combined with the lack of a clearly identifiable pattern in error
+#            messages means that rayvn cannot distinguish between true errors and normal output.
+#
+#            Top level code (i.e. code not in a function) is therefore strongly discouraged, and
+#            any initialization code should be confined to _init_${project}_${library} functions
+#            where normal error handling can occur.
+
 # TODO replace me!
-my${libraryNameInitialCap}LibraryFunction () {
+my${libraryNameInitialCap}LibraryFunction() {
     _my${libraryNameInitialCap}PrivateFunction
 }
 
