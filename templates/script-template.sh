9953f00e0ddab6692a82e5d8b4df097e36cc3302
phynix <phynix@phoggy.me>
2026-01-23 14:20:21 -0800

    Cleanup.

--- templates/script-template.sh
+++ templates/script-template.sh
@@ -32,7 +32,7 @@ init() {
             --version) projectVersion ${quotedName} true; exit 0 ;;
             --debug) setDebug showOnExit ;;
             --debug-new) setDebug clearLog showOnExit ;;
-            --debug-out) setDebug tty "${terminal}" ;;
+            --debug-out) setDebug tty "${ tty; }" ;;
             --debug-tty) shift; setDebug tty "${1}" ;;
             *) usage "Unknown option: ${1}" ;;
         esac
