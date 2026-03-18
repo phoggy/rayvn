87d7a6878b26c5716df77db03a545c1f41698f0c
phynix <phynix@phoggy.me>
2026-03-17 18:10:11 -0700

    Update debug option names.

--- templates/script-template.sh
+++ templates/script-template.sh
@@ -38,8 +38,8 @@ init() {
             -h | --help) usage ;;
             -v) projectVersion ${quotedName}; exit 0 ;;
             --version) projectVersion ${quotedName} true; exit 0 ;;
-            --debug) setDebug showOnExit ;;
-            --debug-new) setDebug clearLog showOnExit ;;
+            --debug) setDebug showLogOnExit ;;
+            --debug-new) setDebug clearLog showLogOnExit ;;
             --debug-out) setDebug tty "${ tty; }" ;;
             --debug-tty) shift; setDebug tty "${1}" ;;
             *) usage "Unknown option: ${1}" ;;
